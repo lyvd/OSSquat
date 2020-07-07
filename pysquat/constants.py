3de80bc7c169b9006663a971f5277e8e3cf7dc45
vuduclyunitn <ducly.vu@unitn.it>
2020-07-07 22:18:03 +0200

    Adds some constants

---
+++ pysquat/constants.py
@@ -0,0 +1,11 @@
+"""
+This file contains constants taken from
+https://github.com/squattingapp/AppCrazy/blob/master/appcrazy/deformation_method.py
+"""
+vowel = ['a', 'e', 'o', 'i', 'u']
+punctuation = [' ', '-', '_']
+r_punctuation = ['.', ' ', '_']
+common_letter_replace_dic = {'b':'p','i':'1,l',"l":'i,1',"o":'0','c':'k','k':'c','p':'b','m':'n','n':'m','s':'5'}
+python_prefix = ['python', 'dev', 'python3', 'git']
+python_suffix = ['python', 'dev', 'python3', 'git']
+
